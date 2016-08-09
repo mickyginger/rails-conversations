1d12da433578966bbd364d8fa3670299f2f11cb6
mickyginger <mickyginger@gmail.com>
2016-08-09 22:07:13 +0100

    Setting up AWS and fog with CarrierWave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
-  storage :file
-
   def store_dir
     "uploads/"
   end
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,15 @@
+# sample config/initializers/carrierwave.rb
+CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
+      :region                 => 'eu-west-1'
+    }
+    config.fog_directory = ENV['AWS_BUCKET_NAME']
+  end
+end
