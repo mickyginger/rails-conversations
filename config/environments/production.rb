0885b93feac4d090652ef1b740acda5fe388a419
mickyginger <mickyginger@gmail.com>
2016-08-09 22:28:47 +0100

    Updated production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,9 +35,12 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.mount_path = nil
+  config.action_cable.url = "wss://rails-conversations.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = [
+    'https://rails-conversations.herokuapp.com/',
+    /http:\/\/rails-conversations.herokuapp.*/
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -83,10 +86,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.web_socket_server_url = "wss://rails-conversations.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = [
-    'https://rails-conversations.herokuapp.com/',
-    'http://rails-conversations.herokuapp.com/'
-  ]
 end
