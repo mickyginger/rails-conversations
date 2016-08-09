d30a2f27766b6aae6f5bd6d0e6ab59e9b0bc6324
mickyginger <mickyginger@gmail.com>
2016-08-09 22:22:06 +0100

    Added websocket server url to production config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.web_socket_server_url = "wss://rails-conversations.herokuapp.com/cable" 
   config.action_cable.allowed_request_origins = [
     'https://rails-conversations.herokuapp.com/',
     'http://rails-conversations.herokuapp.com/'
